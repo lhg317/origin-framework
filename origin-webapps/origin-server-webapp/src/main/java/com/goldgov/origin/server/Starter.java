a44b5ff90afa7c957c9901dc7e93dc8724548b06
LiuHG <niubaodi@sina.com>
2017-07-18 09:57:02 +0800

    注册服务启动类更换目录

--- origin-webapps/origin-server-webapp/src/main/java/com/goldgov/origin/server/Starter.java
+++ origin-webapps/origin-server-webapp/src/main/java/com/goldgov/origin/server/Starter.java
@@ -1,4 +1,4 @@
-package com.goldgov.origin.webapp.server;
+package com.goldgov.origin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
