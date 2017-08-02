2cc1881289b390949bd762372189df7678a47086
LiuHG <niubaodi@sina.com>
2017-08-02 15:00:38 +0800

    修改banner，简化属性文件配置

--- origin-webapps/origin-webgate-webapps/src/main/java/com/goldgov/origin/webgate/Starter.java
+++ origin-webapps/origin-webgate-webapps/src/main/java/com/goldgov/origin/webgate/Starter.java
@@ -3,15 +3,8 @@ package com.goldgov.origin.webgate;
 import org.springframework.boot.SpringApplication;
 
 import com.goldgov.origin.core.OriginApplication;
-import com.goldgov.origin.core.discovery.IsWebGate;
 
-
-/**
- * 如果部署到Weblogic，你的启动类必须直接实现WebApplicationInitializer，即使OriginApplication已经间接实现了此接口
- * @author LiuHG
- * @version 1.0
- */
-public class Starter extends OriginApplication implements IsWebGate{
+public class Starter extends OriginApplication{
 	
     public static void main( String[] args ) {
     	SpringApplication.run(new Object[]{Starter.class}, args);
--- origin-webapps/origin-webgate-webapps/startup.bat
+++ origin-webapps/origin-webgate-webapps/startup.bat
@@ -1,4 +1,4 @@
 cd target
-java -jar origin-webgate-webapps-1.0.0-SNAPSHOT.jar
+java -jar base-service-server.jar
 
 pause
