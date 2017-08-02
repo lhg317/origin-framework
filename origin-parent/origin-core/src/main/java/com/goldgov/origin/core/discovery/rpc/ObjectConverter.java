c84fd8559726b7f5bdb46d6f4182e1cdd0a76c85
LiuHG <niubaodi@sina.com>
2017-08-02 18:51:17 +0800

    为类型转换器增加工具类，负责id数组与List的转换，日期与long的转换

--- origin-parent/origin-core/src/main/java/com/goldgov/origin/core/discovery/rpc/ObjectConverter.java
+++ origin-parent/origin-core/src/main/java/com/goldgov/origin/core/discovery/rpc/ObjectConverter.java
@@ -1,7 +1,31 @@
 package com.goldgov.origin.core.discovery.rpc;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
 public interface ObjectConverter<T,R> {
 
+	public static final String[] PK_ARRAY_TYPE = new String[0];
+	
 	public R toRpcObject(T obj);
 	public T fromRpcObject(R rpcObj);
+	
+	public static class Utils{
+		public static String[] listToArray(List<String> list){
+			return list.toArray(PK_ARRAY_TYPE);
+		}
+		
+		public static List<String> arrayToList(String[] array){
+			return Arrays.asList(array);
+		}
+		
+		public static long dateToLong(Date date){
+			return date.getTime();
+		}
+		
+		public static Date longToDate(long dateLong){
+			return new Date(dateLong);
+		}
+	}
 }
