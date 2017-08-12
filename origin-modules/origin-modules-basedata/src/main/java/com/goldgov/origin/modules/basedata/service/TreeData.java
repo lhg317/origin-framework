abbf3f9573e61ece61964af28ed0a6657f3c3b37
LiuHG <niubaodi@sina.com>
2017-08-12 18:58:28 +0800

    修改TreeData对EndPoint判断的错误

--- origin-modules/origin-modules-basedata/src/main/java/com/goldgov/origin/modules/basedata/service/TreeData.java
+++ origin-modules/origin-modules-basedata/src/main/java/com/goldgov/origin/modules/basedata/service/TreeData.java
@@ -65,7 +65,7 @@ public class TreeData {
 		}
 		
 		subDataMap.put(treeData, treeData.getSubDataList());
-		if(treeData.getSubDataList() == null || treeData.getSubDataList().size() < 0){
+		if(treeData.getSubDataList() == null || treeData.getSubDataList().size() == 0){
 			endPointDataList.add(treeData);
 		}
 	}
