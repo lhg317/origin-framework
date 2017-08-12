10527c92ea7b218280cd8e0b91e8678d373d20d9
LiuHG <niubaodi@sina.com>
2017-08-12 19:54:33 +0800

    附件关联ID改为groupID

--- origin-modules/origin-modules-file/src/main/java/com/goldgov/origin/modules/file/service/FileService.java
+++ origin-modules/origin-modules-file/src/main/java/com/goldgov/origin/modules/file/service/FileService.java
@@ -9,7 +9,7 @@ public interface FileService {
 	
 	void deleteFile(String[] ids);
 	
-	void deleteFileByRelationID(String relationID);
+	void deleteFileByRelationID(String groupID);
 	
 	File getFile(String fileID);
 	
@@ -17,6 +17,6 @@ public interface FileService {
 	
 	List<File> listFile(String groupID);
 	
-	void upldateRelationID(String relationID,String[] fileIDs);
+	void upldateRelationID(String groupID,String[] fileIDs);
 	
 }
