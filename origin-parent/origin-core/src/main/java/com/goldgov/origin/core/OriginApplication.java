691b199a3526f16bd287466e307c5594621ce764
LiuHG <niubaodi@sina.com>
2017-08-24 15:15:20 +0800

    应用正常启动后，打印输出网站的访问地址

--- origin-parent/origin-core/src/main/java/com/goldgov/origin/core/OriginApplication.java
+++ origin-parent/origin-core/src/main/java/com/goldgov/origin/core/OriginApplication.java
@@ -1,22 +1,35 @@
 package com.goldgov.origin.core;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.util.StopWatch;
 import org.springframework.util.StringUtils;
 
+import com.goldgov.origin.core.utils.SpringBeanUtils;
+
 @SpringBootApplication
 @ComponentScan({"com.goldgov.origin"})
 public abstract class OriginApplication extends SpringBootServletInitializer{
 
+	@Value("${server.port:80}")
+	private static int port;
+	
+	@Value("${server.context-path:/}")
+	private static String contextPath;
 	/**
 	 * 用于扩展添加多个配置类，覆盖本方法时，不用考虑当前类加入进配置类中，默认是将本类作为配置类强制加入的。
 	 * 该方法默认什么也不做。
@@ -62,6 +75,8 @@ public abstract class OriginApplication extends SpringBootServletInitializer{
 		message.append(stopWatch.getTotalTimeSeconds());
 		message.append(" seconds.");
 		message.append(getPid());
+		message.append(". ");
+		message.append(getHttpPath());
 		System.out.println(message);
 	}
 	
@@ -70,6 +85,34 @@ public abstract class OriginApplication extends SpringBootServletInitializer{
 			@Override
 			public Object call() throws Exception {
 				return System.getProperty("PID");
+			}
+		});
+	}
+	
+	private static String getHttpPath(){
+		return getValue(" Web Path: ", new Callable<Object>() {
+			@Override
+			public Object call() throws Exception {
+				ServerProperties serverProp = SpringBeanUtils.getBean(ServerProperties.class);
+				StringBuilder webPath = new StringBuilder();
+				String protocol = serverProp.getSsl() == null ? "http://":"https://";
+				Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+				while(networkInterfaces.hasMoreElements()){
+					NetworkInterface nwInterface = networkInterfaces.nextElement();
+					if (nwInterface.isLoopback() || nwInterface.isVirtual() || !nwInterface.isUp()) {
+				          continue;
+				    }else{
+				    	Enumeration<InetAddress> addresses = nwInterface.getInetAddresses();
+				          while (addresses.hasMoreElements()) {
+				        	InetAddress ip = addresses.nextElement();
+				            if (ip != null && ip instanceof Inet4Address) {
+				            	webPath.append(protocol + ip.getHostAddress() + ":" + serverProp.getPort() + (serverProp.getContextPath()==null?"":serverProp.getContextPath()));
+								webPath.append(" ");
+				            }
+				        }
+				    }
+				}
+			    return webPath.toString();
 			}
 		});
 	}
